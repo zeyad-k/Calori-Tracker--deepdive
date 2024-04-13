3ddcd7d01691acc84d505d99cbe3f77a0a6fc3fa
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-04-14 00:12:18 +0200

    Refactor ErrorPage component to add countdown redirect functionality

--- pages/ErrorPage.jsx
+++ pages/ErrorPage.jsx
@@ -1,11 +1,38 @@
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const HOME_LINK = "/";
+const REDIRECT_COUNT = 10;
+const COUNT_DOWN_INTERVAL = 1000;
 
 export function ErrorPage() {
+  const [redirectCounter, setRedirectCounter] = useState(REDIRECT_COUNT);
+
+  const intervalHandler = useRef();
+  const navigateToHome = useNavigate();
+
+  useEffect(() => {
+    if (redirectCounter === 0) {
+      clearInterval(intervalHandler.current);
+      navigateToHome(HOME_LINK);
+    }
+  }, [redirectCounter]);
+
+  useEffect(() => {
+    intervalHandler.current = setInterval(() => {
+      setRedirectCounter((prev) => prev - 1);
+    }, COUNT_DOWN_INTERVAL);
+    return () => {
+      clearInterval(intervalHandler.current);
+    };
+  }, []);
+
   return (
     <div>
       <h1>Something went wrong...</h1>
+      <p>Redirecting to Home Page in {redirectCounter} secondes</p>
       <p>
-        Click <Link to="/">Home Page</Link> to go back
+        Click <Link to={HOME_LINK}>Home Page</Link> to go back
       </p>
     </div>
   );
